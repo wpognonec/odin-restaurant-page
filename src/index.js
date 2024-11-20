ae3aaa0f4366c37b69f445e199744fcbc4efa4db
William Pognonec <william.pognonec@gmail.com>
2024-11-20 09:47:25 -0600

    Add button event listeners

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
-import { greeting } from "./greeting.js";
+import { home } from "./pages/home.js";
+import { menu } from "./pages/menu.js";
+import { about } from "./pages/about.js";
 import "./styles.css"
 
-console.log(greeting);
+const app = document.querySelector("#content")
+const nav = document.querySelector("nav")
+
+nav.addEventListener("click", (e) => {
+    if (e.target instanceof HTMLButtonElement) {
+        console.log("clicked", e.target.id);
+        switch (e.target.id) {
+            case "home":
+                app.textContent = home
+                break;
+            case "menu":
+                app.textContent = menu
+                break;
+            case "about":
+                app.textContent = about
+                break;
+            default:
+                break;
+        }
+    }
+})
