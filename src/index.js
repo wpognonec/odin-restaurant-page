5d12716cf8183ce3f9ef35646cb7b550ab1e33a8
William Pognonec <william.pognonec@gmail.com>
2024-11-20 09:47:25 -0600

    Formatting / Linting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import home from "./pages/home.js";
-import menu from "./pages/menu.js";
-import about from "./pages/about.js";
+import home from "./pages/home.js"
+import menu from "./pages/menu.js"
+import about from "./pages/about.js"
 // import createElement from "./element.js";
 import "./styles.css"
 
@@ -8,23 +8,23 @@ const app = document.querySelector("#content")
 const nav = document.querySelector("nav")
 
 nav.addEventListener("click", (e) => {
-    if (e.target instanceof HTMLButtonElement) {
-        console.log("clicked", e.target.id);
-        switch (e.target.id) {
-            case "home":
-                app.textContent = ""
-                app.appendChild(home)
-                break
-            case "menu":
-                app.textContent = ""
-                app.appendChild(menu)
-                break
-            case "about":
-                app.textContent = ""
-                app.appendChild(about)
-                break
-            default:
-                break;
-        }
+  if (e.target instanceof HTMLButtonElement) {
+    console.log("clicked", e.target.id)
+    switch (e.target.id) {
+      case "home":
+        app.textContent = ""
+        app.appendChild(home)
+        break
+      case "menu":
+        app.textContent = ""
+        app.appendChild(menu)
+        break
+      case "about":
+        app.textContent = ""
+        app.appendChild(about)
+        break
+      default:
+        break
     }
+  }
 })
